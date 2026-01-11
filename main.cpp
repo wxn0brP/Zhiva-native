a8e5018fc4492b7b3bf66262c1779d9a3c412630
wxn0brP <toja.filiper@gmail.com>
2026-01-11 01:37:01 +0100

    fix: windows

--- main.cpp
+++ main.cpp
@@ -112,6 +112,14 @@ int main(int argc, char *argv[]) {
 
     AppContext context = { w };
 
+#if defined(_WIN32)
+    HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(1));
+    if (hIcon) {
+        SendMessage((HWND)webview_get_window(w), WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+        SendMessage((HWND)webview_get_window(w), WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+    }
+#endif
+
 #ifdef WEBVIEW_GTK
     gtk_window_maximize(GTK_WINDOW(webview_get_window(w)));
 #elif defined(_WIN32)
